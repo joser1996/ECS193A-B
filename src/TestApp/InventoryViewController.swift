9ae312f3de3c6b20d77aa9d4e91a222c465b697d
j-a-smith <jasmith2016@yahoo.com>
2020-04-29 14:54:52 -0700

    Add functionality to load PNGs into inventory view

--- src/TestApp/InventoryViewController.swift
+++ src/TestApp/InventoryViewController.swift
@@ -19,29 +19,6 @@ class InventoryViewController: UIViewController, UICollectionViewDataSource, UIC
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "\(BASE_SERVER_URL)/fetch-inventory-items/5/jacob/")
-        guard let requestUrl = url else { fatalError() }
-        var request = URLRequest(url: requestUrl)
-        request.httpMethod = "GET"
-
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-
-            if let error = error {
-                print(error)
-                return
-            }
-
-            do {
-                if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
-                    self.items = convertedJsonIntoDict["items"] as! [String]
-                }
-            } catch let error as NSError {
-                print(error.localizedDescription)
-            }
-            
-        }
-        task.resume()
-        
         collectionView.delegate = self
         collectionView.dataSource = self
     }
@@ -83,13 +60,10 @@ class InventoryViewController: UIViewController, UICollectionViewDataSource, UIC
      
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! InventoryViewCell
-    
-        cell.backgroundColor = self.randomColor()
         
         let index = indexPath[0] + indexPath[1]
         cell.label.text = items[index]
-        
-        print(indexPath)
+        cell.loadThumbnailImage(baseUrlString: BASE_SERVER_URL, item: items[index])
         
         return cell
     }
@@ -114,11 +88,6 @@ class InventoryViewController: UIViewController, UICollectionViewDataSource, UIC
                 print(error)
                 return
             }
-            
-//            // Read HTTP Response Status code
-//            if let response = response as? HTTPURLResponse {
-//                print("Response HTTP Status code: \(response.statusCode)")
-//            }
             
             // Convert HTTP Response Data to a simple String
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
